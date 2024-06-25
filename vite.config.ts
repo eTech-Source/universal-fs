d4a3756b8fd1be510dddc7ff9a18db8c784785a2
eTech-Source <goetch777@gmail.com>
2024-06-25 15:51:13 -0700

    tests(relay-server): add vitest workspaces

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import {defineConfig} from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.UNIVERSAL_FS_PASSWORD) {
+  throw new Error(
+    "Could not get UNIVERSAL_FS_PASSWORD from .env for vite config"
+  );
+}
 
 export default defineConfig({
   server: {
     watch: {
       ignored: ["node_modules", "dist"]
+    }
+  },
+  test: {
+    env: {
+      UNIVERSAL_FS_PASSWORD: process.env.UNIVERSAL_FS_PASSWORD
     }
   }
 });
